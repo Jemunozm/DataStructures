b6d0dc6e51c61f2b402a423acf4d9bb40bd46ff7
Julián Gallego <jugallegog@unal.edu.co>
2024-03-03 12:23:21 -0500

    The first iteration of agenda class was created and a few files were untracked

--- Practicas/Lab3/agenda.py
+++ Practicas/Lab3/agenda.py
@@ -14,10 +14,28 @@ class Agenda:
         self.__no_reg = 0
 
     def agregar(self, usuario: Usuario) -> bool:
-        pass
+        if usuario not in self.__registro and self.__no_reg < self.__capacity:
+            self.__registro[self.__no_reg + 1] = usuario
+        else:
+            print("No fue posible agregar el usuario")
+            return False
 
     def buscar(self, id: int) -> int:
-        pass
+        for i in range(self.__no_reg):
+            if self.__registro[i] == id:
+                return i
+        if id not in self.__registro:
+            return int("-1")
 
     def eliminar(self, id: int) -> bool:
         pass
+
+    def toFile():
+        for i in range(self.__no_reg):
+            with open("agenda.txt", "a") as f:
+                f.append(self.__registro[i] + "\n")
+
+    def importt():
+        with open("agenda.txt", "r") as f:
+            for line in f.read():
+                agregar(line)
