778b41113b4004f6dc78d53127997c71928a9fd9
Julián Gallego <jugallegog@unal.edu.co>
2024-03-02 20:40:06 -0500

    A program was created to use the three last created classes

---
+++ Practicas/Lab2/programa1.py
@@ -0,0 +1,24 @@
+from usuario import Usuario
+from fecha import Fecha
+from direccion import Direccion
+
+
+def main():
+    Fecha1 = Fecha(2, 6, 1999)
+    Direccion1 = Direccion(
+        "78", "32-78", "Belén", "Medellín", "Apartamento 401", "Edificio Niágara"
+    )
+    Julian_data = Usuario(
+        "Julián",
+        11527717436,
+        Fecha1,
+        "Bello, Antioquia",
+        3114290433,
+        "jugallegog@unal.edu.co",
+        Direccion1,
+    )
+    return print(Julian_data)
+
+
+if __name__ == "__main__":
+    main()
