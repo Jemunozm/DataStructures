44838ec0dc72eb203025bdf93838cd0bceabe877
Jesus420 <jemunozm@unal.edu.co>
2024-04-24 16:16:57 -0500

    Refactor code to improve performance and readability

---
+++ Lab5/Direccion.py
@@ -0,0 +1,47 @@
+class Direccion:
+    def __init__(self, calle:str, nomenclatura: str, barrio: str, ciudad: str, edificio: str, apto: str):
+        self.calle = calle
+        self.nomenclatura = nomenclatura
+        self.barrio = barrio
+        self.ciudad = ciudad
+        self.edificio = edificio
+        self.apto = apto
+
+    def setCalle(self, calle:str):
+        self.calle = calle
+    
+    def setNomenclatura(self, nomenclatura:str):
+        self.nomenclatura = nomenclatura
+    
+    def setBarrio(self, barrio:str):    
+        self.barrio = barrio
+
+    def setCiudad(self, ciudad:str):
+        self.ciudad = ciudad
+
+    def setEdificio(self, edificio:str):
+        self.edificio = edificio
+
+    def setApt(self, apto:str):
+        self.apto = apto
+
+    def getCalle(self):
+        return self.calle
+    
+    def getNomenclatura(self):  
+        return self.nomenclatura
+    
+    def getBarrio(self):
+        return self.barrio
+    
+    def getCiudad(self):
+        return self.ciudad
+    
+    def getEdificio(self):
+        return self.edificio
+    
+    def getApto(self):
+        return self.apto
+    
+    def __str__(self):
+        return f"Calle {self.calle} No. {self.nomenclatura} Barrio {self.barrio} Ciudad de {self.ciudad} Edificio {self.edificio} APTO {self.apto}"
