37802c309fffb661d1db40e6f745bc3f223dc67a
JesusHouse <jemunozm@unal.edu.co>
2024-07-25 00:25:23 -0500

    se modificó el metodo para ordenar (aun no se consigue que funcione para el programa 1, en especifico porque aun no esta diseñado para listas enlazadas dobles)

--- Lab5/ColeccionUsuarios.py
+++ Lab5/ColeccionUsuarios.py
@@ -6,24 +6,25 @@ class ColeccionUsuarios:
         self._UsuariosList = ListDouble()
 
     def merge(self, L, R):
-            i = j = 0
-            if i < L.size() and j < R.size():
+        i = j = 0
+        k = 0
+        while k < self._UsuariosList.size:
+            if i < L.currentPositionNode() and j < R.currentPositionNode(): 
                 if L.get(i).getData().getId() < R.get(j).getData().getId():
                     self.addLast(L.get(i))
                     i += 1
+                    k += 1
                 else:
                     self.addLast(R.get(j))
                     j += 1
-            return self._UsuariosList
+                    k += 1
+        return self._UsuariosList
 
-    def mergeSort(self):
-        if self._UsuariosList.size > 1:
-            mid = self._UsuariosList.size // 2
-            left = self._UsuariosList.slice(0, mid)
-            right = self._UsuariosList.slice(mid, self._UsuariosList.size)
-
-            L = self.mergeSort(left)
-            R = self.mergeSort(right)
+    def mergeSort(self, inicio, final):
+        mid = final // 2
+        if inicio < mid:
+            L = self.mergeSort(0, mid)
+            R = self.mergeSort(mid,final)
             return self.merge(L, R)
         else:
             return self._UsuariosList
