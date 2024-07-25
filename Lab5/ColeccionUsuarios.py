ac1d21f4314c52451daba91224356e1ead221773
JesusHouse <jemunozm@unal.edu.co>
2024-07-25 07:18:47 -0500

    Nuevo metodo merge ya enfocado a listas enlazadas dobles

--- Lab5/ColeccionUsuarios.py
+++ Lab5/ColeccionUsuarios.py
@@ -5,27 +5,44 @@ class ColeccionUsuarios:
     def __init__(self):
         self._UsuariosList = ListDouble()
 
-    def merge(self, L, R):
-        i = j = 0
-        k = 0
-        while k < self._UsuariosList.size:
-            if i < L.currentPositionNode() and j < R.currentPositionNode(): 
-                if L.get(i).getData().getId() < R.get(j).getData().getId():
-                    self.addLast(L.get(i))
-                    i += 1
-                    k += 1
-                else:
-                    self.addLast(R.get(j))
-                    j += 1
-                    k += 1
-        return self._UsuariosList
-
-    def mergeSort(self, inicio, final):
-        mid = final // 2
-        if inicio < mid:
-            L = self.mergeSort(0, mid)
-            R = self.mergeSort(mid,final)
-            return self.merge(L, R)
+    def splitList(self,head):
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+        slow.next = None
+        if mid:
+            mid.prev = None
+        return head, mid
+    
+    def merge(self, first, second):
+        if not first:
+            return second
+        if not second:
+            return first
+        
+        if first.data.id <= second.data.id:
+            first.next = self.merge(first.next, second)
+            if first.next:
+                first.next.prev = first
+            first.prev = None
+            return first
         else:
-            return self._UsuariosList
+            second.next = self.merge(first, second.next)
+            if second.next:
+                second.next.prev = second
+            second.prev = None
+            return second
+
+    def mergeSort(self, head):
+        if not head or not head.next:
+            return head
+        
+        first, second = self.splitList(head)
+        first = self.mergeSort(first)
+        second = self.mergeSort(second)
+
+        return self.merge(first, second)
         
