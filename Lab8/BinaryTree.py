7c785c98624eb7c4eb152fea32d6e4380af05ad8
Jesus420 <jemunozm@unal.edu.co>
2024-09-06 11:55:08 -0500

    Update BinaryTree.py

--- Lab8/BinaryTree.py
+++ Lab8/BinaryTree.py
@@ -60,4 +60,85 @@ class BinaryTree:
         else: 
             h = 0
             h = max(self.height(self.left(node)),self.height(self.right(node)))
-            return 1 + h
+            return 1 + h 
+        
+    def addRoot(self,e):
+        self.root = NodeDouble(e)
+        self.size = 1
+    
+    def insertLeft(self, v=NodeDouble, e=object):
+        nodeLeft = NodeDouble(e)
+        v.setLeft(nodeLeft)
+        self.size += 1
+
+    def insertRight(self, v=NodeDouble, e=object):
+        nodeRight = NodeDouble(e)
+        v.setRight(nodeRight)
+        self.size += 1
+
+    def remove(self, v=NodeDouble):
+        p = self.Parent(v)
+        # v tiene al menos un hijo - caso 1
+        if self.hasLeft(v) or self.hasRight(v):
+            if self.hasLeft(v):
+                child = self.left(v)
+            else:
+                child = self.right(v)
+            # Se conecta el hijo de v al padre
+            if self.left(p) == v:
+                p.setLeft(child)
+            else:
+                p.setNext(child)
+            #se conecta al nodo v
+            v.setLeft(None)
+            v.setRight(None)
+        #si v no tiene hijos - caso 2
+        else:
+            if self.left(p)==v:
+                p.setLeft(None)
+            else:
+                p.setRight(None)
+        self.size -= 1
+
+    def visit(self,v=NodeDouble, caso=int):
+        if caso==1:
+            file = open("archivo1", "w")
+            file.write(v.getData())
+            file.close()
+        elif caso ==2:
+            acumulador = 0
+            acumulador += v.getData()
+        else:
+            print(v.getData())
+
+    def Preorder(self, v=NodeDouble):
+        self.visit(v)
+        if self.hasLeft(v):
+            self.Preorder(self,self.left(v))
+        if self.hasRight(v):
+            self.Preorder(self,self.right(v))
+
+    def Inorder(self, v=NodeDouble):
+        if self.hasLeft(v):
+            self.Inorder(self,self.left(v))
+        self.visit(v)
+        if self.hasRight(v):
+            self.Ineorder(self,self.right(v))
+    
+    def Posorder(self, v=NodeDouble):
+        if self.hasLeft(v):
+            self.Posorder(self, self.left(v))
+        if self.right(v):
+            self.Posorder(self, self.right(v))
+        self.visit(v)
+    
+    def minB(self,v=NodeDouble):
+        if self.hasLeft(v):
+            return min(self, self.hasLeft(v))
+        else:
+            return v.getData()
+    def maxB(self,v=NodeDouble):
+        if self.hasRight(v):
+            return min(self, self.hasRight(v))
+        else:
+            return v.getData()
