6950b67f1f828c9a3bc6458763ca0c9d17ee3102
Jesus <jemunozm@unal.edu.co>
2024-09-09 10:33:16 -0500

    SE corrigen errores de importacion y tambien de lagunos metodos que no iban dentro de la clase

--- Lab8/BSTEntry.py
+++ Lab8/BSTEntry.py
@@ -1,35 +1,22 @@
-from Lab8 import BinaryTree
-from Lab8 import NodeDouble
+from BinaryTree import BinaryTree
+from NodeDouble import NodeDouble
 
-class BSTEntry(BinaryTree):
+class BSTEntry:
 
     def __init__(self, e = object, k = int):
         self._data = e
         self._k = k
 
     def getData(self):
-        return self.data
+        return self._data
     
     def setData(self,d=object):
-        self.data = d
+        self._data = d
     
     def getKey(self):
-        return self.k
+        return self._k
     
     def setKey(self, k = int):
-        self.k = k
+        self._k = k
 
-    def searchTree(self, k = int, v = NodeDouble):
-        u = BSTEntry
-        u = v.getData()
-        if k==u.getKey():
-            return v
-        elif k < u.getKey():
-            return self.searchTree(k,v.getLeft())
-        else:
-            return self.datasearchTree(k,v.getRight())
-
-
-    def find(self, k = int):
-        return searchTree(self, k, root)
     
