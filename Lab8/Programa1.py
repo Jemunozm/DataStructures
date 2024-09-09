652c3888bbf8673fb95e5cc9a13188b8ff6ba3b8
Jesus <jemunozm@unal.edu.co>
2024-09-09 10:34:26 -0500

    Se empieza a desarrollar un programa para usar la clase BinarySearchEntry

---
+++ Lab8/Programa1.py
@@ -0,0 +1,14 @@
+from BinarySearchTree import BinarySearchTree
+from BinaryTree import BinaryTree
+from NodeDouble import NodeDouble
+from BSTEntry import BSTEntry
+
+n = BinaryTree()
+
+materiales = BinarySearchTree(n)
+
+
+v = NodeDouble("llave")
+o = BSTEntry("caja",100)
+
+materiales.addEntry(v,o)
