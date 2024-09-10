834af6769d6515e4585093942f8f70a45379d538
JesusHouse <jemunozm@unal.edu.co>
2024-09-10 02:25:52 -0500

    Update BinaryTree.py

--- Lab8/BinaryTree.py
+++ Lab8/BinaryTree.py
@@ -1,4 +1,4 @@
-from NodeDouble import NodeDouble
+from Node import Node
 from Queue import Queue
 
 class BinaryTree:
@@ -15,31 +15,31 @@ class BinaryTree:
     def isEmpty(self):
         return self.size == 0
     
-    def isRoot(self, node):
+    def isRoot(self, node = Node):
         return node == self.root
     
-    def left(self, node):
+    def left(self, node = Node):
         return node.getLeft()
     
-    def right(self, node):
+    def right(self, node = Node):
         return node.getRight()
     
-    def hasLeft(self, node):
+    def hasLeft(self, node = Node):
         return node.getLeft()!=None
 
-    def hasRight(self, node):
+    def hasRight(self, node = Node):
         return node.getRight()!=None
  
-    def isInternal(self, node):
+    def isInternal(self, node = Node):
         return self.hasLeft(node) or self.hasRight(node)
     
-    def Parent(self, node):
+    def Parent(self, node = Node):
         if self.isRoot(node):
             return None
         else:
             Q = Queue()
             Q.enqueue(self.root)
-            temp = NodeDouble(self.root)
+            temp = Node(self.root)
         while (not Q.isEmpty() & self.left(Q.first()) != node & self.right(Q.first()) != node):
             temp = Q.dequeue()
             if self.hasLeft(temp):
@@ -52,7 +52,7 @@ class BinaryTree:
         if self.isRoot(node):
             return 0
         else:
-            return 1 + self.depth(self.parent(node))
+            return 1 + self.depth(self.Parent(node))
     
     def height(self, node):
         if not (self.isInternal(node)):
@@ -63,21 +63,22 @@ class BinaryTree:
             return 1 + h 
         
     def addRoot(self,e):
-        self.root = NodeDouble(e)
+        self.root = Node(e)
         self.size = 1
     
-    def insertLeft(self, v=NodeDouble, e=object):
-        nodeLeft = NodeDouble(e)
+    def insertLeft(self, v=Node, e=object):
+        nodeLeft = Node(e)
         v.setLeft(nodeLeft)
         self.size += 1
 
-    def insertRight(self, v=NodeDouble, e=object):
-        nodeRight = NodeDouble(e)
+    def insertRight(self, v=Node, e=object):
+        nodeRight = Node(e)
         v.setRight(nodeRight)
         self.size += 1
 
-    def remove(self, v=NodeDouble):
-        p = self.Parent(v)
+    def remove(self, v = Node):
+        p = Node(self.Parent(v))
+        #p = self.Parent(v)
         # v tiene al menos un hijo - caso 1
         if self.hasLeft(v) or self.hasRight(v):
             if self.hasLeft(v):
@@ -100,7 +101,7 @@ class BinaryTree:
                 p.setRight(None)
         self.size -= 1
 
-    def visit(self,v=NodeDouble, caso=int):
+    def visit(self,v=Node, caso=int):
         if caso==1:
             file = open("archivo1", "w")
             file.write(v.getData())
@@ -111,33 +112,33 @@ class BinaryTree:
         else:
             print(v.getData())
 
-    def preorder(self, v=NodeDouble):
+    def preorder(self, v=Node):
         self.visit(v)
         if self.hasLeft(v):
             self.preorder(self,self.left(v))
         if self.hasRight(v):
             self.preorder(self,self.right(v))
 
-    def inorder(self, v=NodeDouble):
+    def inorder(self, v=Node):
         if self.hasLeft(v):
             self.inorder(self,self.left(v))
         self.visit(v)
         if self.hasRight(v):
             self.inorder(self,self.right(v))
     
-    def posorder(self, v=NodeDouble):
+    def posorder(self, v=Node):
         if self.hasLeft(v):
             self.posorder(self, self.left(v))
         if self.right(v):
             self.posorder(self, self.right(v))
         self.visit(v)
     
-    def minB(self,v=NodeDouble):
+    def minB(self,v=Node):
         if self.hasLeft(v):
             return min(self, self.hasLeft(v))
         else:
             return v.getData()
-    def maxB(self,v=NodeDouble):
+    def maxB(self,v=Node):
         if self.hasRight(v):
             return min(self, self.hasRight(v))
         else:
